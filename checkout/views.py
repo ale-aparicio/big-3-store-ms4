e7faffd7aab93f7def6d5753b7394687db56164b
ale-aparicio <ale.aparicio@live.com>
2021-11-23 00:28:30 +0000

    Added Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JjkFdBcl3k8zAkhxIUf36i9JFHb1JwWQva2Vn3zAmR9v6kq5e8peR7zLvIz3DDCwHGHmohlaPzSugGTMrAHOegl00LLuyjXhI',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
