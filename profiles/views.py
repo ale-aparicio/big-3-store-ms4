dd4000ed8a06eac9f30d28b6561f804e3577a305
ale-aparicio <ale.aparicio@live.com>
2021-11-30 06:47:54 +0000

    Bug Fixes to the Contact Functions

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, reverse, render, get_object_or_404
 from django.contrib import messages
+from django.core.mail import EmailMessage
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import UserProfile
+from django.conf import settings
 from .forms import UserProfileForm, ContactForm
 
 from checkout.models import Order
