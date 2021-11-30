c0292f7cf64a14748ae2d00f38f3a1403484d5dc
ale-aparicio <ale.aparicio@live.com>
2021-11-30 07:59:37 +0000

    Removed secret key and set debug

--- big_3/settings.py
+++ big_3/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(s^z(t$)_po9lf(!g_5bn3&qb&hxfc%++^uz)rc=_k_-+mhf_n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['big-3-store.herokuapp.com', 'localhost']
 
