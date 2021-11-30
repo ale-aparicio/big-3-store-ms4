660146b0eebe07af41f87f63ff55e8133be76f15
ale-aparicio <ale.aparicio@live.com>
2021-11-30 09:37:48 +0000

    Added Cache Control

--- big_3/settings.py
+++ big_3/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(s^z(t$)_po9lf(!g_5bn3&qb&hxfc%++^uz)rc=_k_-+mhf_n')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -179,6 +179,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+        AWS_S3_OBJECT_PARAMETERS = {
+            'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+            'CacheControl': 'max-age=94608000',
+        }
+
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'big-3-store'
     AWS_S3_REGION_NAME = 'eu-west-3'
